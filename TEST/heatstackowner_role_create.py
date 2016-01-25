7988f73c61891711337334629ce3ea144084ca3c
root <root@kulcloud.(none)>
2016-01-25 16:16:08 +0900

    create heatstackowner role script updated

---
+++ TEST/heatstackowner_role_create.py
@@ -0,0 +1,25 @@
+from keystoneclient.v2_0 import client as keystone_client
+
+
+#change this to your configuration
+username='admin'
+password='kulcloud'
+tenant_name='admin'
+auth_url = 'http://10.1.100.27:35357/v2.0'
+#openstack clients
+keystone =  keystone_client.Client(username=username,
+                                   password=password,
+                                   tenant_name=tenant_name,
+                                   auth_url=auth_url)
+
+roles = keystone.roles.list()
+not_exist = True
+for r in roles:
+    if r.name == 'heat_stack_owner':
+        not_exist = True
+
+if not_exist:
+    keystone.rioles.create('heat_stack_owner')
+
+###########################################
+#import pdb; pdb.set_trace()
